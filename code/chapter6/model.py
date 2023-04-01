87dac4da84a66b471f53217989ede9bed9a22c44
Deploy from CI <>
2023-04-01 07:45:50 +0000

    Deploy b50d5f647595d9955a90f63b3ee3a76af9cea592 to gh-pages

---
+++ code/chapter6/model.py
@@ -0,0 +1,89 @@
+import math
+import tensorflow as tf
+import tensorflow_recommenders as tfrs
+
+class BaseModel(tfrs.models.Model):
+    def __init__(self):
+        super().__init__()
+        self.task = tfrs.tasks.Ranking(
+            loss=tf.keras.losses.BinaryCrossentropy(
+                reduction=tf.keras.losses.Reduction.NONE
+            ),
+            metrics=[
+                tf.keras.metrics.BinaryCrossentropy(name="label-crossentropy"),
+                tf.keras.metrics.AUC(name="auc"),
+                tf.keras.metrics.AUC(curve="PR", name="pr-auc"),
+                tf.keras.metrics.BinaryAccuracy(name="accuracy"),
+            ],
+            prediction_metrics=[
+                tf.keras.metrics.Mean("prediction_mean"),
+            ],
+            label_metrics=[
+                tf.keras.metrics.Mean("label_mean")
+            ]
+        )
+    
+    def call(self, inputs):
+        raise NotImplementedError
+
+    def compute_loss(self, inputs, training=False):
+        features, labels = inputs
+        outputs = self(features, training=training)
+        # loss = tf.reduce_mean(label_loss)
+        loss = self.task(labels=labels["label"], predictions=outputs["label"])
+        loss = tf.reduce_mean(loss)
+        return loss
+
+class Model(BaseModel):
+    def __init__(self, config):
+        super().__init__()
+        self.config = config
+        self.embedding_optimizer = tf.keras.optimizers.legacy.Adam(learning_rate = config.model.learning_rate)
+        self.optimizer = tf.keras.optimizers.Adam(learning_rate = config.model.learning_rate)
+        embedding_layer_feature_config = {}
+        for feature in self.config.model.features:
+            initializer = tf.initializers.TruncatedNormal(
+                mean=0.0, stddev=1 / math.sqrt(feature.embedding_dim)
+            )
+            embedding_layer_feature_config[feature.name] = tf.tpu.experimental.embedding.FeatureConfig(
+                table=tf.tpu.experimental.embedding.TableConfig(
+                vocabulary_size=feature.vocab_size,
+                initializer=initializer,
+                dim=feature.embedding_dim))
+        self.embedding_layer = tfrs.layers.embedding.TPUEmbedding(
+            feature_config=embedding_layer_feature_config,
+            optimizer=self.embedding_optimizer)
+        self.final_activation = tf.keras.layers.Activation('sigmoid')
+        
+
+    def call(self, inputs):
+        embeddings = self.embedding_layer(inputs)
+        user_embs = []
+        movie_embs = []
+        for feature in self.config.model.features:
+            embedding = embeddings[feature.name]
+            if feature.belongs_to == "user":
+                user_embs.append(embedding)
+            elif feature.belongs_to == "movie":
+                movie_embs.append(embedding)
+            else:
+                assert False
+        user_final = tf.concat(user_embs, axis = 1)
+        movie_final = tf.concat(movie_embs, axis = 1)
+        # last unit of embedding is considered to be bias
+        # out = tf.keras.backend.batch_dot(user_final[:, :-1], post_final[:, :-1]) + user_final[:, -1:] +  post_final[:, -1:]
+        # This tf.slice code helps get read of "WARNING:tensorflow:AutoGraph could not transform ..." warnings produced by the above line
+        # doesn't seem to improve speed though
+        # user_final_emb = tf.slice(user_final, begin=[0, 0], size=[user_final.shape[0],  user_final.shape[1] - 1])
+        # user_final_bias = tf.slice(user_final, begin=[0, user_final.shape[1] - 1], size=[user_final.shape[0],  1])
+        # movie_final_emb = tf.slice(movie_final, begin=[0, 0], size=[movie_final.shape[0],  movie_final.shape[1] - 1])
+        # movie_final_bias = tf.slice(movie_final, begin=[0, movie_final.shape[1] - 1], size=[movie_final.shape[0],  1])
+        user_final_emb = user_final[:,:-1]
+        user_final_bias = user_final[:,-1:]
+        movie_final_emb = movie_final[:,:-1]
+        movie_final_bias = movie_final[:,-1:]
+        out = tf.keras.backend.batch_dot(user_final_emb, movie_final_emb) + user_final_bias + movie_final_bias
+        prediction = self.final_activation(out) 
+        return {
+            "label": prediction
+        }
---
+++ code/chapter6/train.py
@@ -0,0 +1,59 @@
+import argparse
+from config import Config
+from dataset import create_dataset
+from model import Model
+import tensorflow as tf
+import sys
+print("tf.__version__: ", tf.__version__)
+
+def save_string_gcs(string_object, gcs_dir, filename):
+    string_string = json.dumps(string_object)
+    with open(filename, "w") as f:
+        f.write(string_string)
+    os.system(f"gsutil -m cp {filename} {gcs_dir}/{filename}")
+    os.system(f"rm {filename}")
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--config", type=str, required=True, help="Path to config")
+    parser.add_argument("--tpu_name", type=str)
+    parser.add_argument("--tpu_zone", type=str)
+    args = parser.parse_args()
+    config = Config(args.config)
+    if args.tpu_name:
+        assert args.tpu_zone
+        handle = tf.distribute.cluster_resolver.TPUClusterResolver(tpu=args.tpu_name, zone=args.tpu_zone)
+        tpu = handle.connect(args.tpu_name, args.tpu_zone)
+        strategy = tf.distribute.TPUStrategy(tpu)
+        tf.tpu.experimental.initialize_tpu_system(handle)
+        print("num_replicas_in_sync: ", strategy.num_replicas_in_sync)
+    else:
+        strategy = tf.distribute.get_strategy()
+    train_dataset = create_dataset(config, strategy, config.train_path)
+
+    with strategy.scope():
+        model = Model(config)
+        model.compile(model.optimizer, steps_per_execution=1)
+        train_dataset = create_dataset(config, strategy, config.train_path)
+        trainval_dataset = create_dataset(config, strategy, config.validate_path)
+        eval_dataset = create_dataset(config, strategy, config.validate_path)
+        train_steps_per_epoch = config.train_rows // config.global_batch_size
+        trainval_steps_per_epoch = config.trainval_rows // config.global_batch_size
+        eval_steps_per_epoch = config.eval_rows // config.global_batch_size
+        checkpoints_cb = tf.keras.callbacks.ModelCheckpoint(config.save_model_path  + '/checkpoints/',  save_freq = train_steps_per_epoch//3)
+        callbacks=[checkpoints_cb]
+        history = model.fit(train_dataset, epochs=config.epochs, callbacks=[callbacks], steps_per_epoch=train_steps_per_epoch,
+        validation_data=trainval_dataset, validation_steps=trainval_steps_per_epoch)
+        model.save_weights(config.save_model_path  + '/weights/')
+        eval_steps = config.eval_rows // config.global_batch_size
+        eval_scores = model.evaluate(eval_dataset, return_dict=True, steps=eval_steps_per_epoch)
+        metrics = {}
+        metrics["eval"] = eval_scores
+        metrics["history"] = history.history
+        metrics["args"] = sys.argv
+        metrics["config"] = repr(config)
+        save_string_gcs(json.dumps(metrics), config.save_model_path, f"metrics_pretrain.json")
+
+
+if __name__ == "__main__":
+    main()
